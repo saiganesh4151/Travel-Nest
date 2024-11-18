82a581da73a89effba1cb94a77a53906beb5b19b
SaiGanesh <saiganeshambati000@gmail.com>
2024-11-19 00:35:31 +0530

    reviews route

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
-
+const Review=require("./review.js");
 
 
 const listingSchema=new Schema(
@@ -32,7 +32,21 @@ const listingSchema=new Schema(
         country :
         {
             type : String
-        }
+        },
+        reviews :
+        [
+            {
+                type : Schema.Types.ObjectId,
+                ref : "Review"
+            }
+        ]
+
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing && listing.reviews) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
 });
 
 const Listing=mongoose.model("Listing",listingSchema);
